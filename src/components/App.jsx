3460c1409269ef79643e2f8db4c898930c6a5b5d
Vladyslav <dyadyavadya308@gmail.com>
2023-07-21 18:09:01 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,3 @@
-// import ContactForm from './ContactForm';
-// import Contacts from './Contacts';
-
-// function App() {
-//   return (
-//     <>
-//       <h1>Phonebook</h1>
-//       <br />
-//       <ContactForm />
-//       <br />
-//       <Contacts />
-//     </>
-//   );
-// }
-// export default App;
-
 import { useEffect, lazy } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
